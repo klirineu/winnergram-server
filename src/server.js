f538476a2df376b88efdb0980f9b19d84ad27928
klirineu <klirineu@MacBook-Pro-de-klirineu.local>
2022-05-04 15:11:51 -0300

    make it better

--- src/server.js
+++ src/server.js
@@ -1,4 +1,4 @@
-import { app } from "./app";
+const { app } = require("./app")
 
 app.listen(process.env.HTTPSERVER_PORT, () => {
     console.info("wg:http ➜ listen on *" + process.env.HTTPSERVER_PORT);
