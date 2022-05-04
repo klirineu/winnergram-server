3d52486ce9bbfdcb762ff2781c9ade95fa316a14
klirineu <klirineu@MacBook-Pro-de-klirineu.local>
2022-05-04 18:58:46 -0300

    make it better

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 "use strict";var _app = require('./app');
 
-_app.app.listen(process.env.HTTPSERVER_PORT, () => {
+_app.app.listen(process.env.HTTPSERVER_PORT || 3333, () => {
     console.info("wg:http ➜ listen on *" + process.env.HTTPSERVER_PORT);
 });
